0426e515ced43ef5a174dfa72c9134cef045cf04
Czarek Nakamoto <cyjan@mrcyjanek.net>
2026-03-02 20:49:14 +0100

    make native pkgs unique

--- pack/info.go
+++ pack/info.go
@@ -80,8 +80,13 @@ func (p *Package) GeneratePackageInfo(h *host.Host) string {
 	}
 	env := p.GetEnvForLogs(h)
 	delete(env, "PATH")
+	delete(env, "PREFIX")
 	pkgs["_env"] = env
-	pkgs["_prefix"] = h.GetEnvPath()
+	if (p.Type == "native") {
+		pkgs["_prefix"] = h.GetNativeEnvPath()
+	} else {
+		pkgs["_prefix"] = h.GetEnvPath()
+	}
 	info, err := json.MarshalIndent(pkgs, "", "  ")
 	crash.Handle(err)
 	return string(info)
